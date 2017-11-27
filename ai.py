16045840410285c91a3b6f5e187390a40bdb16d3
Venkatesh tata <venkateshtata@Venkateshs-MacBook-Pro.local>
2017-11-27 19:19:39 +0530

    init2

---
+++ ai.py
@@ -0,0 +1,12 @@
+
+
+import numpy as np 
+import random
+import os
+import torch
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+ 
